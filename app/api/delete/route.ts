8e9bc58e2a5d4700dd19738a5a6886c9d6bfe210
demarcuscode <demarcuscode@Kali.Kali>
2026-02-20 00:11:59 +0000

    added the .nvmrc

--- app/api/delete/route.ts
+++ app/api/delete/route.ts
@@ -1,35 +1,35 @@
-// import { del } from "@vercel/blob";
+// // import { del } from "@vercel/blob";
+// // import { NextResponse } from "next/server";
+
+// // export async function POST(req: Request) {
+// //   const { pathname } = await req.json();
+
+// //   await del(pathname);
+
+// //   return NextResponse.json({ success: true });
+// // }
+
+// import { supabase } from "@/lib/supabase/client";
 // import { NextResponse } from "next/server";
 
 // export async function POST(req: Request) {
 //   const { pathname } = await req.json();
 
-//   await del(pathname);
+//   if (!pathname) {
+//     return NextResponse.json({
+//       success: false,
+//       error: "No file path provided",
+//     });
+//   }
+
+//   const { data, error } = await supabase.storage
+//     .from("scalf.io-bucket") // replace with your bucket name
+//     .remove([pathname]); // remove expects an array of file paths
+
+//   if (error) {
+//     console.error("Error deleting file:", error.message);
+//     return NextResponse.json({ success: false, error: error.message });
+//   }
 
 //   return NextResponse.json({ success: true });
 // }
-
-import { supabase } from "@/lib/supabase/client";
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-  const { pathname } = await req.json();
-
-  if (!pathname) {
-    return NextResponse.json({
-      success: false,
-      error: "No file path provided",
-    });
-  }
-
-  const { data, error } = await supabase.storage
-    .from("scalf.io-bucket") // replace with your bucket name
-    .remove([pathname]); // remove expects an array of file paths
-
-  if (error) {
-    console.error("Error deleting file:", error.message);
-    return NextResponse.json({ success: false, error: error.message });
-  }
-
-  return NextResponse.json({ success: true });
-}
--- app/api/paystack/plan/route.ts
+++ app/api/paystack/plan/route.ts
@@ -1,21 +1,21 @@
-import { paystack } from "@/lib/paystack";
-import { NextRequest, NextResponse } from "next/server";
+// import { paystack } from "@/lib/paystack";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  try {
-    const { email, amount } = await req.json();
+// export async function POST(req: NextRequest) {
+//   try {
+//     const { email, amount } = await req.json();
 
-    const response = await paystack.post("/transaction/initialize", {
-      email,
-      amount,
-    });
+//     const response = await paystack.post("/transaction/initialize", {
+//       email,
+//       amount,
+//     });
 
-    return NextResponse.json(response.data);
-  } catch (error: any) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Failed to initialize transaction" },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(response.data);
+//   } catch (error: any) {
+//     console.error(error);
+//     return NextResponse.json(
+//       { error: "Failed to initialize transaction" },
+//       { status: 500 }
+//     );
+//   }
+// }
